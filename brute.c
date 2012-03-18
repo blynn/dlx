8e63a4fc9f1c33273630db050d61e6f683e97f1d
Ben Lynn <benlynn@gmail.com>
2012-03-18 05:02:22 -0700

    Short and sweet.

--- brute.c
+++ brute.c
@@ -2,79 +2,54 @@
 #include <stdlib.h>
 #include "def.h"
 
-#define REP(i, n) for(int i=0; i<n; i++)
-#define NE(m, n) if (eq(m, n)) return 0;
+#define F(i, n) for(int i=0; i<n; i++)
+#define N(...) if (match((int[]){__VA_ARGS__, -1})) return 0;
 
 int main() {
   char *s[3][6] = { { "Bob", "Chuck",  "Dave", "Ed", "Frank", "Gary"},
                     { "Hall", "King", "Noyes", "Pinza", "Veery", "White"},
                     { "AL", "AR", "AS", "ML", "MR", "MS" } };
-  void x(int *i, int *j) {
-    int t = *i;
-    *i = *j, *j = t;
-  }
-  int a[6][2];
-  int get(int m, int i, int n) {
-    REP(k, 6) if ((m ? a[k][m-1] : k) == i) return a[k][n-1];
+  int a[6][2], swp;
+  void x(int *i, int *j) { swp = *i, *i = *j, *j = swp; }
+  int has(int k, int m, int i) { return (m ? a[k][m-1] : k) == i; }
+  int starter(int m, int i) {
+    F(k, 6) if (has(k, m, i)) return a[k][1]/3;
     fprintf(stderr, "unreachable!\n"), exit(1);
   }
-  int eq(int m, int i, int n, int j) { return get(m, i, n) == j; }
+  int match(int *p) {
+    F(k, 6) for(int n=0, *q=p; *q>=0; q+=2) if ((n += has(k, *q, q[1])) > 1) return 1;
+    return 0;
+  }
   int g() {
     // 1. Every order was different.
     //
     // 2. Bob, King, and a man who ordered ravioli...
     //    Chuck, Hall, and a man who ordered spaghetti...
-    if (get(Bob, 2)/3 != get(King, 2)/3) return 0;
-    if (get(Chuck, 2)/3 != get(Hall, 2)/3) return 0;
-    NE(Bob, King)
-    NE(Bob, Hall);
-    NE(Bob, AR);
-    NE(Bob, MR);
-    NE(King, AR);
-    NE(King, MR);
-    NE(Chuck, Hall);
-    NE(Chuck, King);
-    NE(Chuck, AS);
-    NE(Chuck, MS);
-    NE(Hall, AS);
-    NE(Hall, MS);
+    if (starter(Bob) != starter(King)) return 0;
+    if (starter(Chuck) != starter(Hall)) return 0;
+    N(Bob, King, AR, MR);
+    N(Chuck, Hall, AS, MS);
+    N(Bob, Chuck, Hall, King);
     // 3. Gary and White both ordered lasagna; Hall did not.
-    NE(Gary, AR);
-    NE(Gary, AS);
-    NE(Gary, MR);
-    NE(Gary, MS);
-    NE(White, AR);
-    NE(White, AS);
-    NE(White, MR);
-    NE(White, MS);
-    NE(Hall, AL);
-    NE(Hall, ML);
-    NE(Gary, White);
-    NE(Gary, Hall);
+    N(Gary, White, AR, AS, MR, MS);
+    N(Hall, AL, ML);
+    N(Gary, Hall);
     // 4. Frank didn't order minestrone; neither he nor Pinza ordered ravioli.
-    NE(Frank, ML);
-    NE(Frank, MR);
-    NE(Frank, MS);
-    NE(Frank, AR);
-    NE(Pinza, AR);
-    NE(Pinza, MR);
-    NE(Frank, Pinza);
+    N(Frank, ML, MR, MS);
+    N(Frank, Pinza, AR, MR);
     // 5. Neither Ed nor Frank are Veery.
-    NE(Ed, Veery);
-    NE(Frank, Veery);
+    N(Ed, Frank, Veery);
     return 1;
   }
   int n[2] = { 6, 6 }, p[2][6];
-  REP(i, 2) REP(j, 6) p[i][j] = j;
+  F(i, 2) F(j, 6) p[i][j] = j;
   void f() {
-    int i = 0;
-    while(i < 2 && !n[i]) i++;
-    if (i == 2) {
-      if (g()) REP(k, 6) printf("%s, %s, %s\n", s[0][k], s[1][a[k][0]], s[2][a[k][1]]);
+    F(i, 2) if (n[i]) {
+      int *q = p[i];
+      F(k, n[i]) a[--n[i]][i] = q[k], x(q+k, q+n[i]), f(), x(q+k, q+n[i]++);
       return;
     }
-    int *q = p[i];
-    REP(k, n[i]) a[--n[i]][i] = q[k], x(q+k, q+n[i]), f(), x(q+k, q+n[i]++);
+    if (g()) F(k, 6) printf("%s, %s, %s\n", s[0][k], s[1][*a[k]], s[2][a[k][1]]);
   }
   f();
   return 0;
