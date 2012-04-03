4c87c01088541051e8ad7a2dcc70d6086d8b00d0
Ben Lynn <benlynn@gmail.com>
2012-04-03 15:58:27 -0700

    Cosmetic changes.

--- v2.c
+++ v2.c
@@ -1,137 +1,141 @@
+#define _GNU_SOURCE
+#include <ctype.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "cbt.h"
+#include <string.h>
 #include "darray.h"
 
 #define F(i,n) for(int i = 0; i < n; i++)
 
-struct col_s;
-typedef struct col_s *col_ptr;
+#define VAPRINT(s, fmt) \
+    va_list params; \
+    va_start(params, fmt); \
+    char *s; \
+    vasprintf(&s, fmt, params); \
+    va_end(params);
+
+void die(const char *err, ...) {
+  va_list params;
+
+  va_start(params, err);
+  vfprintf(stderr, err, params);
+  fputc('\n', stderr);
+  va_end(params);
+  exit(1);
+}
+
 struct cell_s;
 typedef struct cell_s *cell_ptr;
 
 struct cell_s {
   cell_ptr U, D, L, R;
-  col_ptr c;
-};
-typedef struct cell_s *cell_ptr;
-
-struct col_s {
-  // These 4 pointers must come first, and in this order.
-  cell_ptr U, D;
-  col_ptr L, R;
-  int s, n;
-  cbt_it it;
+  int n;
+  union {
+    cell_ptr c;
+    int s;
+  };
 };
 
-col_ptr col_new() {
-  col_ptr c = malloc(sizeof(*c));
+cell_ptr col_new() {
+  cell_ptr c = malloc(sizeof(*c));
   c->s = 0;
-  c->U = c->D = (cell_ptr) c;
+  c->U = c->D = c;
   return c;
 }
 
 struct dlx_s {
-  cbt_t ctab, rtab;
-  col_ptr root;
-  cell_ptr cur;
+  darray_ptr ctab, rtab;
+  cell_ptr root;
+  char *(*cid)(int);
 };
 typedef struct dlx_s *dlx_t;
 
+char *default_cid(int n) {
+  static char buf[16];
+  sprintf(buf, "%d", n);
+  return buf;
+}
+
 dlx_t dlx_new() {
   dlx_t p = malloc(sizeof(*p));
-  p->ctab = cbt_new();
-  p->rtab = cbt_new();
-  col_ptr h = col_new();
+  p->ctab = darray_new();
+  p->rtab = darray_new();
+  cell_ptr h = col_new();
   h->L = h->R = h;
   p->root = h;
-  p->cur = 0;
+  p->cid = default_cid;
   return p;
 }
 
-void dlx_add_col(dlx_t p, char *key) {
-  cbt_it it;
-  void *f(void *data) { free(data); return 0; }
-  if (!cbt_insert_with(&it, p->ctab, f, key)) {
-    fprintf(stderr, "duplicate column key: %s\n", key);
-    return;
-  }
-  col_ptr c = col_new();
-  c->U = c->D = (cell_ptr) c;
-  col_ptr h = p->root, last = h->L;
-  h->L = last->R = c, c->L = last, c->R = h;
-  cbt_put(it, c);
-  c->it = it;
-  c->n = cbt_size(p->ctab) - 1;
+void dlx_add_col(dlx_t p) {
+  cell_ptr c = col_new();
+  c->U = c->D = c;
+  cell_ptr h = p->root;
+  c->L = h->L; h->L = h->L->R = c, c->R = h;
+  c->n = darray_count(p->ctab);
+  darray_append(p->ctab, c);
 }
 
-void dlx_dump_col(dlx_t p) {
-  for(col_ptr c = p->root->R; c != p->root; c = c->R) {
-    printf("%s(%d)\n", cbt_key(c->it), c->n);
-  }
+void dlx_add_row(dlx_t p) {
+  darray_append(p->rtab, 0);
 }
 
-void dlx_add_row(dlx_t p, char *rkey, char *ckey) {
-  cbt_it it;
-  void *f(void *data) { free(data); return 0; }
-  if (!cbt_insert_with(&it, p->rtab, f, rkey)) {
-    fprintf(stderr, "duplicate row key: %s\n", rkey);
-    return;
-  }
+void dlx_alloc_col(dlx_t p, int n) {
+  while(darray_count(p->ctab) < n) dlx_add_col(p);
+}
+
+void dlx_alloc_row(dlx_t p, int n) {
+  while(darray_count(p->rtab) < n) dlx_add_row(p);
+}
+
+// Called by dlx_set the first time we add a 1 to a row.
+static cell_ptr row_first(dlx_t p, cell_ptr c, int row) {
   cell_ptr n = malloc(sizeof(*n));
-  cbt_put(it, n);
-  it = cbt_at(p->ctab, ckey);
-  if (cbt_is_off(it)) {
-    fprintf(stderr, "column key not found: %s\n", ckey);
-    return;
-  }
-  col_ptr c = cbt_get(it);
-  cell_ptr last = c->U;
-  c->U = last->D = n;
-  n->U = last;
-  n->D = (cell_ptr) c;
+  n->U = c->U;
+  c->U = c->U->D = n;
+  n->D = n->c = c;
   n->L = n->R = n;
-  n->c = c;
   c->s++;
-  p->cur = n;
+  n->n = row;
+  return n;
 }
 
-void dlx_add_1(dlx_t p, char *key) {
-  if (!p->cur) {
-    fprintf(stderr, "no rows yet");
+void dlx_set(dlx_t p, int row, int col) {
+  if (row >= darray_count(p->rtab)) dlx_alloc_row(p, row + 1);
+  if (col >= darray_count(p->ctab)) dlx_alloc_col(p, col + 1);
+  cell_ptr c = (cell_ptr) darray_at(p->ctab, col);
+  cell_ptr *rp = (cell_ptr *) p->rtab->item + row;
+  if (!*rp) {
+    *rp = row_first(p, c, row);
     return;
   }
-  cbt_it it = cbt_at(p->ctab, key);
-  if (cbt_is_off(it)) {
-    fprintf(stderr, "column key not found: %s\n", key);
-    return;
-  }
-  col_ptr c = cbt_get(it);
-  if (c->n <= p->cur->c->n) {
+  cell_ptr r =(*rp)->L;
+  if (c->n <= r->c->n) {
     // We enforce this to avoid duplicate columns in a row, and for simplicity,
     // though the algorithm works even if the columns were jumbled.
-    fprintf(stderr, "must add columns in incresaing order: %s\n", key);
-    return;
+    die("must add columns in increasing order: %d");
   }
   cell_ptr n = malloc(sizeof(*n));
-  cell_ptr last = c->U;
-  last->D = c->U = n;
-  n->U = last;
-  n->D = (cell_ptr) c;
-  cell_ptr next = p->cur->R;
-  next->L = p->cur->R = n; 
-  n->L = p->cur;
-  n->R = next;
-  n->c = c;
+  n->U = c->U;
+  c->U = c->U->D = n;
+  n->D = n->c = c;
+  n->R = r->R;
+  r->R = r->R->L = n; 
+  n->L = r;
   c->s++;
-  p->cur = n;
+  r = n;
 }
 
-void cover_col(col_ptr c) {
-//printf("cover %s\n", cbt_key(c->it));
+void dlx_set_last_row(dlx_t p, int col) {
+  dlx_set(p, darray_count(p->rtab) - 1, col);
+}
+
+void cover_col(cell_ptr c) {
+//printf("cover %d\n", cbt_key(c->it));
   c->R->L = c->L;
   c->L->R = c->R;
-  for(cell_ptr i = c->D; i != (cell_ptr) c; i = i->D) {
+  for(cell_ptr i = c->D; i != c; i = i->D) {
     for(cell_ptr j = i->R; j != i; j = j->R) {
       j->U->D = j->D;
       j->D->U = j->U;
@@ -140,9 +144,9 @@ void cover_col(col_ptr c) {
   }
 }
 
-void uncover_col(col_ptr c) {
+void uncover_col(cell_ptr c) {
 //printf("uncover %s\n", cbt_key(c->it));
-  for(cell_ptr i = c->U; i != (cell_ptr) c; i = i->U) {
+  for(cell_ptr i = c->U; i != c; i = i->U) {
     for(cell_ptr j = i->L; j != i; j = j->L) {
       j->c->s++;
       j->U->D = j->D->U = j;
@@ -151,7 +155,10 @@ void uncover_col(col_ptr c) {
   c->R->L = c->L->R = c;
 }
 
-static void cover_row(cell_ptr r) {
+void dlx_cover_row(dlx_t p, int i) {
+  if (i >= darray_count(p->rtab)) die("%d out of range", i);
+  cell_ptr r = p->rtab->item[i];
+  if (!r) return;
   cover_col(r->c);
   for(cell_ptr j = r->R; j != r; j = j->R) {
     cover_col(j->c);
@@ -159,11 +166,10 @@ static void cover_row(cell_ptr r) {
 }
 
 void dlx_search(dlx_t p) {
-  col_ptr h = p->root;
+  cell_ptr h = p->root;
   darray_ptr sol = darray_new();
   void search(int k) {
-//printf("search %d\n", k);
-    col_ptr c = h->R;
+    cell_ptr c = h->R;
     if (c == h) {
       // Print solution.
       printf("solution:\n");
@@ -171,7 +177,7 @@ void dlx_search(dlx_t p) {
         cell_ptr r = data;
         while(r->L->c->n < r->c->n) r = r->L;
         do {
-          printf(" %s", cbt_key(r->c->it));
+          printf(" %s", p->cid(r->c->n));
           r = r->R;
         } while (r->R->c->n > r->c->n);
         printf("\n");
@@ -180,13 +186,13 @@ void dlx_search(dlx_t p) {
       return;
     }
     int s = c->s;
-    for(col_ptr cc = c->R; cc != h; cc = cc->R) {
+    for(cell_ptr cc = c->R; cc != h; cc = cc->R) {
       if (cc->s < s) c = cc, s = c->s;
     }
-printf("lvl %d: %s(%d)\n", k, cbt_key(c->it), s);
+printf("lvl %d(%d): %s\n", k, s, p->cid(c->n));
     if (!s) return;
     cover_col(c);
-    for(cell_ptr r = c->D; r != (cell_ptr) c; r = r->D) {
+    for(cell_ptr r = c->D; r != c; r = r->D) {
       darray_append(sol, r);
       for(cell_ptr j = r->R; j != r; j = j->R) {
         cover_col(j->c);
@@ -204,62 +210,76 @@ printf("lvl %d: %s(%d)\n", k, cbt_key(c->it), s);
 
 int main() {
   static int grid[9][9];
-  char s[80];
   F(i, 9) {
-    // TODO: malloc getline
-    if (EOF == scanf("%s\n", s)) {
-      fprintf(stderr, "input error\n");
-      exit(128);
-    }
+    char *s = 0;
+    size_t len = 0;
+    if (-1 == getline(&s, &len, stdin)) die("input error");
     F(j, 9) {
       if (s[j] == '\n') break;
-      grid[i][j] = s[j] >= '1' && s[j] <= '9' ? s[j] - '0' : 0;
+      grid[i][j] = isdigit(s[j]) ? s[j] - '0' : 0;
     }
   }
 
   dlx_t dlx = dlx_new();
-  // One digit per box.
-  F(r, 9) F(c, 9) {
-    sprintf(s, "%d,%d", r, c);
-    dlx_add_col(dlx, s);
-  }
-  // Once per row.
-  F(r, 9) F(n, 9) {
-    sprintf(s, "%dr%d", n, r);
-    dlx_add_col(dlx, s);
-  }
-  // Once per column.
-  F(n, 9) F(c, 9) {
-    sprintf(s, "%dc%d", n, c);
-    dlx_add_col(dlx, s);
-  }
-  // Once per 3x3 region.
-  F(n, 9) F(r, 3) F(c, 3) {
-    sprintf(s, "%d %d,%d", n, r, c);
-    dlx_add_col(dlx, s);
-  }
-  // Add rows:
-  F(n, 9) F(r, 9) F(c, 9) {
-    char key[16];
-    sprintf(s, "%d %d %d", n, r, c);
-    sprintf(key, "%d,%d", r, c);
-    dlx_add_row(dlx, s, key);
-    sprintf(key, "%dr%d", n, r);
-    dlx_add_1(dlx, key);
-    sprintf(key, "%dc%d", n, c);
-    dlx_add_1(dlx, key);
-    sprintf(key, "%d %d,%d", n, r/3, c/3);
-    dlx_add_1(dlx, key);
+
+  darray_ptr con = darray_new();
+  darray_ptr choice = darray_new();
+
+  void constrain(const char *id, ...) {
+    VAPRINT(s, id);
+    darray_append(con, s);
+    dlx_add_col(dlx);
   }
 
-  for (int i = 0; i < 9; i++) {
-    for (int j = 0; j < 9; j++) {
-      if (grid[i][j]) {
-        sprintf(s, "%d %d %d", grid[i][j] - 1, i, j);
-        cover_row((cell_ptr) cbt_get(cbt_at(dlx->rtab, s)));
-      }
-    }
+  void new_row(const char *id, ...) {
+    VAPRINT(s, id);
+    darray_append(choice, s);
+    dlx_add_row(dlx);
   }
+
+  void add_1(const char *id, ...) {
+    VAPRINT(s, id);
+    F(c, darray_count(con)) if (!strcmp(s, con->item[c])) {
+      dlx_set_last_row(dlx, c);
+      free(s);
+      return;
+    }
+    die("no such contraint %s", s);
+  }
+
+  void choose(const char *id, ...) {
+    VAPRINT(s, id);
+    F(i, darray_count(choice)) if (!strcmp(s, choice->item[i])) {
+      dlx_cover_row(dlx, i);
+      free(s);
+      return;
+    }
+    die("no such choice %s", s);
+  }
+
+  char *constraint_print(int n) { return con->item[n]; }
+  dlx->cid = constraint_print;
+
+  // Setup human-friendly names for columns.
+  // Exactly one digit per box.
+  F(r, 9) F(c, 9) constrain("! %d %d", r, c);
+  // Each digit appears exactly once per row.
+  F(n, 9) F(r, 9) constrain("%d r %d", n, r);
+  // ...per column.
+  F(n, 9) F(c, 9) constrain("%d c %d", n, c);
+  // ... and per 3x3 region.
+  F(n, 9) F(r, 3) F(c, 3) constrain("%d x %d %d", n, r, c);
+
+  // Add rows:
+  F(n, 9) F(r, 9) F(c, 9) {
+    new_row("%d @ %d %d", n, r, c);
+    add_1("! %d %d", r, c);
+    add_1("%d r %d", n, r);
+    add_1("%d c %d", n, c);
+    add_1("%d x %d %d", n, r/3, c/3);
+  }
+
+  F(r, 9) F(c, 9) if (grid[r][c]) choose("%d @ %d %d", grid[r][c] - 1, r, c);
 
   dlx_search(dlx);
   return 0;
