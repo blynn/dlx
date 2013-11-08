17fa2e7ac171c9b1b3dd5e24243074136a704e1c
Ben Lynn <benlynn@gmail.com>
2013-11-08 00:02:32 -0800

    Cleaned up v2.c.
    Removed dependency on darray.
    
    Added dependency on BLT.

--- v2.c
+++ v2.c
@@ -1,13 +1,26 @@
 // Solves logic grid puzzles using the DLX algorithm.
+//
+// We view a logic grid puzzle as follows. Given a MxN table of objects and
+// some constraints, for each row except the first, we are to permute its
+// entries so that the table satisfies the constraints.
+//
+// The simplest constraints specify that 2 particular objects must lie in
+// the same column, or lie in different columns. More complex ones involve
+// ordering.
+//
+// DLX also involves rows and columns. To avoid confusion, we'll use the
+// terms DLX-rows and DLX-columns for these.
 #define _GNU_SOURCE
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "blt.h"
 #include "dlx.h"
-#include "darray.h"
 
 #define F(i, n) for(int i=0; i<n; i++)
+
+#define GROW(A, N, MAX) if (N == MAX) A = realloc(A, sizeof(*A) * (MAX *= 2))
 
 #define NORETURN __attribute__((__noreturn__))
 void die(const char *err, ...) NORETURN __attribute__((format (printf, 1, 2)));
@@ -41,43 +54,39 @@ char *mallocgets() {
 
 struct hint_s {
   char cmd;
-  int i[8], j[8], k[8], n;
+  int i[8], j[8], k[8], n, dlx_col;
 };
 typedef struct hint_s *hint_ptr;
 
-darray_ptr hints;
-
 int main() {
-  int dim = 0, m = 0;
-  char *sym[16][16];
-  hints = darray_new();
-
-  // We view a logic grid puzzle as follows. Given a MxN table of objects and
-  // some constraints, for each row except the first, we are to permute its
-  // entries so that the table satisfies the constraints.
-  //
-  // The simplest constraints specify that 2 particular objects must lie in
-  // the same column, or lie in different columns. More complex ones involve
-  // ordering.
-  //
-  // DLX also involves rows and columns. To avoid confusion, we'll use the
-  // terms DLX-rows and DLX-columns for these.
-
-  // Expect an MxN table of space-delimited fields, terminated by "%%" on a
+  BLT *blt = blt_new();
+  int M = 0, N = 0;
+  // Read M lines of N space-delimited fields, terminated by "%%" on a
   // single line by itself.
   for(char *s = 0; (s = mallocgets()) && strcmp(s, "%%"); free(s)) {
-    int i = 0;
+    int n = 0;
     void f(char *s) {
-      F(i, dim) F(j, m) if (!strcmp(sym[i][j], s)) die("duplicate symbol: %s", s);
-      F(j, i) if (!strcmp(sym[dim][j], s)) die("duplicate symbol: %s", s);
-      sym[dim][i++] = strdup(s);
+      int *coord = malloc(sizeof(*coord) * 2);
+      coord[0] = M;
+      coord[1] = n++;
+      if (blt_put_if_absent(blt, s, coord)) die("duplicate symbol: %s", s);
     }
     forall_word(s, f);
-    if (!dim) m = i; else if (m != i) die("line %d: wrong number of fields", dim + 1);
-    dim++;
+    if (!M) N = n; else if (N != n) die("line %d: wrong number of fields", M+1);
+    M++;
   }
+  char *sym[M][N];
+  int add_sym(BLT_IT *it) {
+    int *coord = it->data;
+    sym[coord[0]][coord[1]] = it->key;
+    return 1;
+  }
+  blt_forall(blt, add_sym);
 
   // Expect a list of constraints, one per line.
+  int hint_max = 64;
+  int hint_n = 0;
+  hint_ptr *hint = malloc(sizeof(*hint) * hint_max);
   for(char *s = 0; (s = mallocgets()); free(s)) {
     hint_ptr h = 0;
     void f(char *s) {
@@ -85,7 +94,9 @@ int main() {
         h = malloc(sizeof(*h));
         h->cmd = *s;
         h->n = 0;
-        darray_append(hints, h);
+        h->dlx_col = 0;
+        GROW(hint, hint_n, hint_max);
+        hint[hint_n++] = h;
         return;
       }
       h->k[h->n] = 0;
@@ -94,164 +105,131 @@ int main() {
         *d = 0;
         h->k[h->n] = atoi(d+1);
       }
-      F(i, dim) F(j, m) if (!strcmp(sym[i][j], s)) {
-        h->i[h->n] = i;
-        h->j[h->n] = j;
-        h->n++;
-        return;
-      }
-      die("invalid symbol: %s", s);
+      int *coord = blt_get(blt, s)->data;
+      if (!coord) die("invalid symbol: %s", s);
+      h->i[h->n] = coord[0];
+      h->j[h->n] = coord[1];
+      h->n++;
     }
     forall_word(s, f);
+    if(h->cmd == '>') {
+      if (h->n != 2) die("inequality must have exactly 2 fields");
+      h->cmd = '<';
+      int tmp;
+      tmp = h->i[0]; h->i[0] = h->i[1], h->i[1] = tmp;
+      tmp = h->j[0]; h->j[0] = h->j[1], h->j[1] = tmp;
+      tmp = h->k[0]; h->k[0] = h->k[1], h->k[1] = tmp;
+    }
   }
 
-  darray_ptr con = darray_new();
-  darray_ptr choice = darray_new();
   dlx_t dlx = dlx_new();
 
-  void constrain(char *s) { darray_append(con, s); }
-  void constrain_opt(char *s) {
-    dlx_mark_optional(dlx, darray_count(con));
-    darray_append(con, s);
-  }
-  void add_1(char *s) {
-    F(c, darray_count(con)) if (!strcmp(s, con->item[c])) {
-      dlx_set(dlx, darray_count(choice) - 1, c);
-      return;
-    }
-    die("no such constraint %s", s);
-  }
-  void new_row(char *s) { darray_append(choice, strdup(s)); }
-
-  // Add one DLX-column for each symbol in our table.
-  F(j, dim) F(k, m) constrain(sym[j][k]);
-  char buf[1024] = { 0 };
-  int a[m];
-
-  char *hsym(hint_ptr p, int n) { return sym[p->i[n]][p->j[n]]; }
-  void optcol(void *data) {
-    hint_ptr p = data;
-    switch(p->cmd) {
-      char *s;
-      // TODO: Below, I should encode p->k[0] too, but no puzzles seem to use
-      // anything except 0 anyway.
-      case '>':
-        if (p->n != 2) die("inequality must have exactly 2 fields");
-        p->cmd = '<';
-        {
-           int tmp;
-           tmp = p->i[0]; p->i[0] = p->i[1], p->i[1] = tmp;
-           tmp = p->j[0]; p->j[0] = p->j[1], p->j[1] = tmp;
-           tmp = p->k[0]; p->k[0] = p->k[1], p->k[1] = tmp;
-        }
-        // FALLTHROUGH
-      case '<':
-      case '1':
-      case 'A':
-        F(k, m) {
-          asprintf(&s, "%s%c%s:%d", hsym(p, 0), p->cmd, hsym(p, 1), k);
-          constrain_opt(s);
-        }
-        break;
-    }
-  }
-  darray_forall(hints, optcol);
-
+  // The first MN DLX-columns represent the symbols.
+  // The symbol at row r and column c corresponds to DLX-column N*r + c.
+  int dlxN = M * N;
+  int dlxM = 0;
+  int dlx_max = 32;
+  int (*dlx_a)[5] = malloc(sizeof(*dlx_a) * dlx_max);
+  int a[M];
   // Generate all possible columns. An M-digit counter in base N.
-  void f(char *s, int i) {
-    int has(hint_ptr p, int i) {return a[p->i[i]] == p->j[i];}
-    int match(hint_ptr p) {
+  void f(int i) {
+    int has(hint_ptr h, int i) { return a[h->i[i]] == h->j[i]; }
+    int match(hint_ptr h) {
       int t = 0;
-      F(n, p->n) t += has(p, n);
+      F(i, h->n) t += has(h, i);
       return t;
     }
-    if (i == dim) {
+    if (i == M) {
       // Base case: finished generating a single column. If contraints allow
       // it, add a DLX-row representing this column, otherwise skip it.
       // The DLX-row has a 1 in the DLX-columns corresponding to the symbols
       // in the column.
-      int anon(void *data) {
-        hint_ptr p = data;
-        switch(p->cmd) {
-          case 'p': return (has(p, 0) && has(p, 1)) ||
-              (has(p, 2) && has(p, 3)) || (match(p) | 2) != 2;
-          case '=': return match(p) == 1;
+      int anon(hint_ptr h) {
+        switch(h->cmd) {
+          case 'h': return (has(h, 0) && has(h, 1)) ||
+              (has(h, 2) && has(h, 3)) || (match(h) | 2) != 2;
+          case '=': return match(h) == 1;
           case '<':
           case '1':
           case 'A':
-          case '!': return match(p) > 1;
-          case 'i': return has(p, 0) && (match(p) | 2) != 2;
-          /*
-          case '^': return matchcount(p) >= 2;
-        */
+          case '!': return match(h) > 1;
+          case 'i': return has(h, 0) && (match(h) | 2) != 2;
+          // TODO: case '^': return matchcount(h) >= 2;
         }
         return 0;
       }
-      if (darray_until(hints, anon)) return;
-      new_row(buf);
-      F(k, dim) add_1(sym[k][a[k]]);
-      // Some constraints require extra, optional columns to enforce.
-      void opthints(void *data) {
-        hint_ptr p = data;
-        switch(p->cmd) {
-          char *s;
+      F(i, hint_n) if (anon(hint[i])) return;
+      // No constraints immediately disqualify this column.
+      // Add a new DLX-row to represent it.
+      GROW(dlx_a, dlxM, dlx_max);
+      F(i, M) dlx_a[dlxM][i] = a[i];
+      // Set the DLX-column coresponding to each symbol.
+      F(k, M) dlx_set(dlx, dlxM, N*k + a[k]);
+      // Add optional columns for constraints that need it.
+      void assign_dlx_col(hint_ptr h) {
+        if (!h->dlx_col) {
+          h->dlx_col = dlxN;
+          F(i, N) dlx_mark_optional(dlx, dlxN++);
+        }
+      }
+      void opthints(hint_ptr h) {
+        switch(h->cmd) {
           case '1':
-            if (has(p, 0)) {
-              F(k, m) {
-                if (k == a[p->k[0]] + 1) continue;
-                asprintf(&s, "%s%c%s:%d", hsym(p, 0), p->cmd, hsym(p, 1), k);
-                add_1(s), free(s);
+            assign_dlx_col(h);
+            if (has(h, 0)) {
+              F(k, N) {
+                if (k == a[h->k[0]] + 1) continue;
+                dlx_set(dlx, dlxM, h->dlx_col + k);
               }
             }
-            if (has(p, 1)) {
-              asprintf(&s, "%s%c%s:%d", hsym(p, 0), p->cmd, hsym(p, 1), a[p->k[0]]);
-              add_1(s), free(s);
+            if (has(h, 1)) {
+              dlx_set(dlx, dlxM, h->dlx_col + a[0]);
             }
             break;
           case 'A':
-            if (has(p, 0)) {
-              F(k, m) {
-                if (abs(k - a[p->k[0]]) == 1) continue;
-                asprintf(&s, "%s%c%s:%d", hsym(p, 0), p->cmd, hsym(p, 1), k);
-                add_1(s), free(s);
+            assign_dlx_col(h);
+            if (has(h, 0)) {
+              F(k, N) {
+                if (abs(k - a[h->k[0]]) == 1) continue;
+                dlx_set(dlx, dlxM, h->dlx_col + k);
               }
             }
-            if (has(p, 1)) {
-              asprintf(&s, "%s%c%s:%d", hsym(p, 0), p->cmd, hsym(p, 1), a[p->k[0]]);
-              add_1(s), free(s);
+            if (has(h, 1)) {
+              dlx_set(dlx, dlxM, h->dlx_col + a[0]);
             }
             break;
           case '<':
-            if (has(p, 0)) {
-              for(int k = 0; k <= a[p->k[0]]; k++) {
-                asprintf(&s, "%s%c%s:%d", hsym(p, 0), p->cmd, hsym(p, 1), k);
-                add_1(s), free(s);
+            assign_dlx_col(h);
+            if (has(h, 0)) {
+              for(int k = 0; k <= a[h->k[0]]; k++) {
+                dlx_set(dlx, dlxM, h->dlx_col + k);
               }
             }
-            if (has(p, 1)) {
-              for(int k = a[p->k[1]]; k < m; k++) {
-                asprintf(&s, "%s%c%s:%d", hsym(p, 0), p->cmd, hsym(p, 1), k);
-                add_1(s), free(s);
+            if (has(h, 1)) {
+              for(int k = a[h->k[1]]; k < N; k++) {
+                dlx_set(dlx, dlxM, h->dlx_col + k);
               }
             }
             break;
         }
       }
-      darray_forall(hints, opthints);
+      F(i, hint_n) opthints(hint[i]);
+      dlxM++;
       return;
     }
-    if (i) *s++ = ' ';
-    F(k, m) {
+    F(k, N) {
       a[i] = k;
-      strcpy(s, sym[i][k]);
-      f(s + strlen(sym[i][k]), i+1);
+      f(i+1);
     }
   }
-  f(buf, 0);
+  f(0);
 
   // Solve!
   void pr(int row[], int n) {
-    F(i, n) printf("%s\n", (char *) choice->item[row[i]]);
+    F(i, n) {
+      F(k, M) printf(" %s", sym[k][dlx_a[row[i]][k]]);
+      putchar('\n');
+    }
   }
   dlx_forall_cover(dlx, pr);
   dlx_clear(dlx);
